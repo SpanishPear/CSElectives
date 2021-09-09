a76dbe5d0ab550adbb277b3f6af1bd4ae7038fdf
auvictoriavu <auvictoriavu@gmail.com>
2021-09-09 13:38:27 +1000

    working on changing bar charts to circles

--- src/components/review-card-ratings-only.js
+++ src/components/review-card-ratings-only.js
@@ -4,39 +4,87 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Grid, Card, Progress, Rating } from 'semantic-ui-react';
+import ReviewRating from '../components/review-rating/review-rating.js';
 
 const RatingsCard = (props) => {
   const { reviewTitle, usefulProgress, workloadProgress, difficultyProgress,
-    enjoymentProgress, reviewDate, overallRating } = props;
+    enjoymentProgress, reviewDate, overallRating, termTaken, author } = props;
 
   return (
-    <div style={{ display: 'block', margin: '20px', width: '100%' }}>
+
+    <div className='review-cards'>
       <Card fluid raised>
         <Card.Content>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Card.Header as='h3' content={reviewTitle} />
-                <div>
-                  <label>Usefulness</label>
-                  <Progress value={usefulProgress} total='5' progress='ratio' size='small' color='blue' />
-                </div>
-                <div>
-                  <label>Workload</label>
-                  <Progress value={workloadProgress} total='5' progress='ratio' size='small' color='blue' />
-                </div>
+          <Grid columns={2}>
+            <Grid.Row className='review-row'>
+              <Grid.Column>
+                <Card.Header><h3>{reviewTitle}</h3></Card.Header>
+                <Card.Meta className='course-review-card-star-rating'>
+                  Overall:
+                  <Rating icon='star' rating={overallRating} maxRating={5} disabled />
+                  <div>
+                    Term taken: {termTaken}
+                  </div>
+                </Card.Meta>
 
               </Grid.Column>
-              <Grid.Column width={8}>
-                <span className='course-review-card-date'>{reviewDate}</span>
-                <div>
-                  <label>Enjoyment</label>
-                  <Progress value={enjoymentProgress} total='5' progress='ratio' size='small' color='blue' />
-                </div>
-                <div>
-                  <label>Difficulty</label>
-                  <Progress value={difficultyProgress} total='5' progress='ratio' size='small' color='blue' />
-                </div>
+              <Grid.Column>
+                <div style={{ textAlign: 'right' }}>{reviewDate}</div>
+                <Card.Meta className='reviewCardAuthor'>
+                  {author}
+                </Card.Meta>
+
+              </Grid.Column>
+            </Grid.Row>
+
+
+          </Grid>
+
+          <Grid columns={4}>
+            <Grid.Row className='review-row'>
+              <Grid.Column width={3}>
+                <label>Usefulness</label>
+                <br />
+                <label>Workload</label>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <ReviewRating
+                  rating={usefulProgress}
+                  icon='circle'
+                  size='small'
+                  captions={['🤬', '😥', '😐', '😀', '😍']}
+                  onChange={(newRating) => setOverallRating(newRating)}
+                />
+                <ReviewRating
+                  rating={workloadProgress}
+                  icon='circle'
+                  size='small'
+                  captions={['🤬', '😥', '😐', '😀', '😍']}
+                  onChange={(newRating) => setOverallRating(newRating)}
+                />
+
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <label>Enjoyment</label>
+                <br />
+                <label>Difficulty</label>
+
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <ReviewRating
+                  rating={enjoymentProgress}
+                  icon='circle'
+                  size='small'
+                  captions={['🤬', '😥', '😐', '😀', '😍']}
+                  onChange={(newRating) => setOverallRating(newRating)}
+                />
+                <ReviewRating
+                  rating={difficultyProgress}
+                  icon='circle'
+                  size='small'
+                  captions={['🤬', '😥', '😐', '😀', '😍']}
+                  onChange={(newRating) => setOverallRating(newRating)}
+                />
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -56,6 +104,8 @@ RatingsCard.propTypes = {
   difficultyProgress: PropTypes.number,
   reviewDate: PropTypes.number,
   overallRating: PropTypes.number,
+  termTaken: PropTypes.string,
+  author: PropTypes.string,
 };
 
 export default RatingsCard;
--- src/components/temporary.js
+++ src/components/temporary.js
@@ -82,3 +82,159 @@
         </Card.Content>
       </Card>
     </div>
+
+// fixed size
+
+<div style={{ display: 'block', margin: '20px', width: '100%' }}>
+      <Card fluid raised>
+        <Card.Content>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Card.Meta className='course-review-card-star-rating'>
+                  Overall:
+                  <Rating icon='star' rating={overallRating} maxRating={5} disabled />
+                  <div>
+                    Term taken: {termTaken}
+                  </div>
+                </Card.Meta>
+                <Card.Header as='h3' content={reviewTitle} />
+                <div>
+                  <label>Usefulness</label>
+                  <Progress value={usefulProgress} total='5' progress='ratio' size='small' color='blue' />
+                </div>
+                <div>
+                  <label>Workload</label>
+                  <Progress value={workloadProgress} total='5' progress='ratio' size='small' color='blue' />
+                </div>
+
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <span className='course-review-card-date'>{reviewDate}</span>
+                <div>
+                  <label>Enjoyment</label>
+                  <Progress value={enjoymentProgress} total='5' progress='ratio' size='small' color='blue' />
+                </div>
+                <div>
+                  <label>Difficulty</label>
+                  <Progress value={difficultyProgress} total='5' progress='ratio' size='small' color='blue' />
+                </div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Card.Content>
+      </Card>
+    </div>
+
+// dots
+
+<div style={{ display: 'block', margin: '20px', width: '100%' }}>
+      <Card fluid raised>
+        <Card.Content>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Card.Meta className='course-review-card-star-rating'>
+                  Overall:
+                  <Rating icon='star' rating={overallRating} maxRating={5} disabled />
+
+                  <div>
+                    Term taken: {termTaken}
+                  </div>
+        
+                </Card.Meta>
+                <Card.Header as='h3' content={reviewTitle} />
+
+
+
+<Grid>
+                <div>
+                  <Grid columns={2}>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <label>Usefulness</label>
+
+                      </Grid.Column>
+
+                      <Grid.Column>
+                        <ReviewRating
+                          rating={usefulProgress}
+                          icon='circle'
+                          size='small'
+                          captions={['🤬', '😥', '😐', '😀', '😍']}
+                          onChange={(newRating) => setOverallRating(newRating)}
+                        />
+
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <label>Usefulness</label>
+
+                      </Grid.Column>
+
+                      <Grid.Column>
+                        <ReviewRating
+                          rating={usefulProgress}
+                          icon='circle'
+                          size='small'
+                          captions={['🤬', '😥', '😐', '😀', '😍']}
+                          onChange={(newRating) => setOverallRating(newRating)}
+                        />
+
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+
+
+                </div>
+
+
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <div>
+                  <Grid columns={2}>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <label>Usefulness</label>
+
+                      </Grid.Column>
+
+                      <Grid.Column>
+                        <ReviewRating
+                          rating={usefulProgress}
+                          icon='circle'
+                          size='small'
+                          captions={['🤬', '😥', '😐', '😀', '😍']}
+                          onChange={(newRating) => setOverallRating(newRating)}
+                        />
+
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        <label>Usefulness</label>
+
+                      </Grid.Column>
+
+                      <Grid.Column>
+                        <ReviewRating
+                          rating={usefulProgress}
+                          icon='circle'
+                          size='small'
+                          captions={['🤬', '😥', '😐', '😀', '😍']}
+                          onChange={(newRating) => setOverallRating(newRating)}
+                        />
+
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+
+
+                </div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Card.Content>
+      </Card>
+    </div>
